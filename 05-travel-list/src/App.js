727fef1ae774608c83fc1695a976d9835f4c0268
Kadir Karabacak <kadirt@hotmail.com>
2023-12-13 20:57:36 +0300

    Sort feature is added with derived state on form elements.

--- 05-travel-list/src/App.js
+++ 05-travel-list/src/App.js
@@ -89,10 +89,31 @@ function Form({ onAddItems }) {
 
 // List of packing
 function PackingList({ items, onDeleteItem, onToggleItem }) {
+  // Sort state
+  const [sortBy, setSortBy] = useState("input");
+
+  // To handle changed sort state
+  let sortedItems;
+
+  // Sort conditions by select element
+  if (sortBy === "input") sortedItems = items;
+
+  if (sortBy === "description")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => a.description.localeCompare(b.description));
+
+  // To sort by a boolean we have to convert them to numbers, true = 1 / false = 0
+  if (sortBy === "packed")
+    sortedItems = items
+      .slice()
+      .sort((a, b) => Number(b.packed) - Number(a.packed));
+
   return (
     <div className="list">
       <ul>
-        {items.map((item) => (
+        {/* Looping over sorted condition instead of "items" */}
+        {sortedItems.map((item) => (
           <Item
             item={item}
             onDeleteItem={onDeleteItem}
@@ -101,6 +122,15 @@ function PackingList({ items, onDeleteItem, onToggleItem }) {
           />
         ))}
       </ul>
+
+      {/* Sort actions */}
+      <div className="actions">
+        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
+          <option value="input">Sort by input order</option>
+          <option value="description">Sort by description</option>
+          <option value="packed">Sort by packed status</option>
+        </select>
+      </div>
     </div>
   );
 }
