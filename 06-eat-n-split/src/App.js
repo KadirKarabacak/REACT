77c96db713ddd192c58ae2077e72965caed6d1d1
Kadir Karabacak <kadirt@hotmail.com>
2023-12-18 20:56:53 +0300

    Added key prop to SplitBill form to each time reset input fields

--- 06-eat-n-split/src/App.js
+++ 06-eat-n-split/src/App.js
@@ -96,6 +96,7 @@ export default function App() {
         <FormSplitBill
           selectedFriend={selectedFriend}
           onSplitBill={handleSplitBill}
+          key={selectedFriend.id}
         />
       )}
     </div>
