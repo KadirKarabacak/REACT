f3e744d239f4963f202dc4a8f29fa44a9115fc74
Kadir Karabacak <kadirt@hotmail.com>
2024-01-03 18:17:26 +0300

    Code clean

--- 14-redux-intro/src/features/customers/Customer.js
+++ 14-redux-intro/src/features/customers/Customer.js
@@ -2,7 +2,6 @@ import { useSelector } from "react-redux";
 
 function Customer() {
   const customer = useSelector((store) => store.customer);
-  console.log(customer);
   return <h2>👋 Welcome, {customer.fullName} </h2>;
 }
 
