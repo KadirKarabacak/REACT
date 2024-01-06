8bee901a270d29fab58ff2ae6364e6d8a20fcf8b
Kadir Karabacak <kadirt@hotmail.com>
2024-01-06 23:38:51 +0300

    Tailwind styles

--- 15-fast-react-pizza/src/features/order/CreateOrder.jsx
+++ 15-fast-react-pizza/src/features/order/CreateOrder.jsx
@@ -5,28 +5,29 @@ const isValidPhone = (str) =>
     str,
   );
 
-import { Form, redirect, useActionData, useNavigation } from "react-router-dom";
-import { createOrder } from "../../services/apiRestaurant";
-import Button from "../../ui/Button";
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
+import { createOrder } from '../../services/apiRestaurant';
+import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -34,9 +35,11 @@ const fakeCart = [
 ];
 
 function CreateOrder() {
+  // Taking username from store again
+  const username = useSelector((state) => state.user.username);
   // Disable button when it is already submitting
   const navigation = useNavigation();
-  const isSubmitting = navigation.state === "submitting";
+  const isSubmitting = navigation.state === 'submitting';
 
   // To access action data need useActionData() which react-router feature
   const formErrors = useActionData();
@@ -45,33 +48,48 @@ function CreateOrder() {
   const cart = fakeCart;
 
   return (
-    <div>
-      <h2>Ready to order? Lets go!</h2>
+    <div className="px-4 py-6">
+      <h2 className="mb-8 text-xl font-semibold">Ready to order? Lets go!</h2>
 
       {/* We include this "Form" from react-router to create new Order */}
       <Form method="POST">
-        <div>
-          <label>First Name</label>
-          <input className="input" type="text" name="customer" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">First Name</label>
+          <input
+            className="input sm:grow"
+            type="text"
+            name="customer"
+            required
+            defaultValue={username}
+          />
         </div>
 
-        <div>
-          <label>Phone number</label>
-          <div>
-            <input className="input" type="tel" name="phone" required />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Phone number</label>
+          <div className="grow">
+            <input className="input w-full" type="tel" name="phone" required />
+            {/* Render the error comes from action if there is */}
+            {formErrors?.phone && (
+              <p className="mt-2 rounded-md bg-red-100 p-2 text-xs text-red-700">
+                {formErrors.phone}
+              </p>
+            )}
           </div>
-          {/* Render the error comes from action if there is */}
-          {formErrors?.phone && <p>{formErrors.phone}</p>}
         </div>
 
-        <div>
-          <label>Address</label>
-          <div>
-            <input type="text" name="address" required className="input" />
+        <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
+          <label className="sm:basis-40">Address</label>
+          <div className="grow">
+            <input
+              type="text"
+              name="address"
+              required
+              className="input w-full"
+            />
           </div>
         </div>
 
-        <div>
+        <div className="mb-12 flex items-center gap-5">
           <input
             className="h-6 w-6 accent-yellow-400 "
             type="checkbox"
@@ -80,14 +98,16 @@ function CreateOrder() {
             // value={withPriority}
             // onChange={(e) => setWithPriority(e.target.checked)}
           />
-          <label htmlFor="priority">Want to yo give your order priority?</label>
+          <label className="font-medium" htmlFor="priority">
+            Want to yo give your order priority?
+          </label>
         </div>
 
         <div>
           {/* A hidden input to take fakeCart data, doesnt matter where it is */}
           <input type="hidden" name="cart" value={JSON.stringify(cart)} />
-          <Button disabled={isSubmitting}>
-            {isSubmitting ? "Placing order..." : "Order now"}
+          <Button type="primary" disabled={isSubmitting}>
+            {isSubmitting ? 'Placing order...' : 'Order now'}
           </Button>
         </div>
       </Form>
@@ -107,16 +127,14 @@ export async function action({ request }) {
   const order = {
     ...data,
     cart: JSON.parse(data.cart),
-    priority: data.priority === "on",
+    priority: data.priority === 'on',
   };
 
   //! Handle error messages
   const errors = {};
 
   // No valid phone number
-  if (!isValidPhone(order.phone))
-    errors.phone =
-      "Please give us your correct phone number. We might need it to contact you.";
+  if (!isValidPhone(order.phone)) errors.phone = 'Wrong phone number.';
 
   // Errors object has any error, then return it
   if (Object.keys(errors).length > 0) return errors;
