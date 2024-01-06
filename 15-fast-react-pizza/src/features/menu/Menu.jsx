965ca7878890173837fd43ca9f71a50532a63b4a
Kadir Karabacak <kadirt@hotmail.com>
2024-01-07 00:56:59 +0300

    Handled when  there is no user

--- 15-fast-react-pizza/src/features/menu/Menu.jsx
+++ 15-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,12 +1,23 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 import { getMenu } from '../../services/apiRestaurant';
 import MenuItem from './MenuItem';
+import { useSelector } from 'react-redux';
+import { getUserName } from '../user/userSlice';
+import { useEffect } from 'react';
 
 function Menu() {
   // Getting data from the loader in App.jsx
   const menu = useLoaderData();
-  console.log(menu);
+  const username = useSelector(getUserName);
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (!username) navigate('/');
+    },
+    [username, navigate],
+  );
 
   return (
     <ul className="divide-y divide-stone-200 px-2">
