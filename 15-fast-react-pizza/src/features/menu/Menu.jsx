6e34bee87a136804105db05aae9b2e64c01909c8
Kadir Karabacak <kadirt@hotmail.com>
2024-01-06 23:38:31 +0300

    menu styles

--- 15-fast-react-pizza/src/features/menu/Menu.jsx
+++ 15-fast-react-pizza/src/features/menu/Menu.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useLoaderData } from "react-router-dom";
-import { getMenu } from "../../services/apiRestaurant";
-import MenuItem from "./MenuItem";
+import { useLoaderData } from 'react-router-dom';
+import { getMenu } from '../../services/apiRestaurant';
+import MenuItem from './MenuItem';
 
 function Menu() {
   // Getting data from the loader in App.jsx
@@ -9,7 +9,7 @@ function Menu() {
   console.log(menu);
 
   return (
-    <ul>
+    <ul className="divide-y divide-stone-200 px-2">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
