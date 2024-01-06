254e914f224da3bfaf63399a5117e23c24463bab
Kadir Karabacak <kadirt@hotmail.com>
2024-01-07 00:57:32 +0300

    Button onclick features

--- 15-fast-react-pizza/src/ui/Button.jsx
+++ 15-fast-react-pizza/src/ui/Button.jsx
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-function Button({ children, disabled, to, type }) {
+function Button({ children, disabled, to, type, onClick }) {
   const base =
     'inline-block text-sm rounded-full bg-yellow-400 font-semibold uppercase tracking-wide text-stone-800 transition-colors duration-300 hover:bg-yellow-300 focus:bg-yellow-300 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed';
 
@@ -11,12 +11,23 @@ function Button({ children, disabled, to, type }) {
       ' inline-block text-sm rounded-full px-4 py-2.5 md:px-6 md:py-3.5 bg-transparent font-semibold border-2 border-stone-300 uppercase tracking-wide text-stone-400 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-800 focus:bg-stone-300 focus:outline-none focus:ring focus:ring-stone-300 focus:ring-offset-2 disabled:cursor-not-allowed',
   };
 
+  // If there is "to" prop
   if (to)
     return (
       <Link className={styles[type]} to={to}>
         {children}
       </Link>
     );
+
+  // If there is onClick prop
+  if (onClick)
+    return (
+      <button onClick={onClick} disabled={disabled} className={styles[type]}>
+        {children}
+      </button>
+    );
+
+  // If there is nothing
   return (
     <button disabled={disabled} className={styles[type]}>
       {children}
