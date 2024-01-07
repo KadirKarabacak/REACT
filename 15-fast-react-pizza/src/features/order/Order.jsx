e60b47b3ea1cbd5796be136b98f8299f2d7ebfa8
Kadir Karabacak <kadirt@hotmail.com>
2024-01-08 00:00:03 +0300

    Imported UpdateOrder component

--- 15-fast-react-pizza/src/features/order/Order.jsx
+++ 15-fast-react-pizza/src/features/order/Order.jsx
@@ -10,6 +10,7 @@ import {
   formatDate,
 } from '../../utils/helpers';
 import { useEffect } from 'react';
+import UpdateOrder from './UpdateOrder';
 
 function Order() {
   // Using loader data which we create
@@ -96,6 +97,7 @@ function Order() {
           To pay on delivery: {formatCurrency(orderPrice + priorityPrice)}
         </p>
       </div>
+      {!priority && <UpdateOrder order={order} />}
     </div>
   );
 }
