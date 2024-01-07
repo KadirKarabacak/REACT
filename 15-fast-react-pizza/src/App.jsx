36fe46d1456ee7dc531c3dfcace55fdb730a0943
Kadir Karabacak <kadirt@hotmail.com>
2024-01-07 23:59:46 +0300

    Added action to /order/:orderId

--- 15-fast-react-pizza/src/App.jsx
+++ 15-fast-react-pizza/src/App.jsx
@@ -1,16 +1,17 @@
 // A new way of setting Routers
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
 // Components
-import Home from "./ui/Home";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
-import Cart from "./features/cart/Cart";
+import Order, { loader as orderLoader } from './features/order/Order';
+import Menu, { loader as menuLoader } from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import AppLayout from './ui/AppLayout';
+import Home from './ui/Home';
+import Error from './ui/Error';
 import CreateOrder, {
   action as createOrderAction,
-} from "./features/order/CreateOrder";
-import Order, { loader as orderLoader } from "./features/order/Order";
-import AppLayout from "./ui/AppLayout";
-import Error from "./ui/Error";
+} from './features/order/CreateOrder';
+import { action as updateOrderAction } from './features/order/UpdateOrder';
 
 // Using it and selecting paths, each object is one Route
 const router = createBrowserRouter([
@@ -18,25 +19,27 @@ const router = createBrowserRouter([
     element: <AppLayout />,
     errorElement: <Error />,
     children: [
-      { path: "/", element: <Home /> },
+      { path: '/', element: <Home /> },
       // Providing loader to Menu Router
       {
-        path: "/menu",
+        path: '/menu',
         element: <Menu />,
         loader: menuLoader,
         errorElement: <Error />,
       },
-      { path: "/cart", element: <Cart /> },
+      { path: '/cart', element: <Cart /> },
       {
-        path: "/order/new",
+        path: '/order/new',
         element: <CreateOrder />,
         action: createOrderAction,
       },
       {
-        path: "/order/:orderId",
+        path: '/order/:orderId',
         element: <Order />,
         loader: orderLoader,
         errorElement: <Error />,
+        // Updating priority on order page
+        action: updateOrderAction,
       },
     ],
   },
