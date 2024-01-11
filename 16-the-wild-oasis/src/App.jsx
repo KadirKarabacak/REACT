ac4af93d64bd4f7f0783799758d43556ca236ca8
Kadir Karabacak <kadirt@hotmail.com>
2024-01-12 00:08:07 +0300

    Imported whole pages and created routes

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -1,81 +1,42 @@
-import styled from "styled-components";
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+// Pages
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Users from "./pages/Users";
+import Settings from "./pages/Settings";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+
+// Styles
 import GlobalStyles from "./styles/GlobalStyles";
-import Button from "./ui/Button";
-import Input from "./ui/Input";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
+import AppLayout from "./ui/AppLayout";
 
-// const H1 = styled.h1`
-//   font-size: 30px;
-//   font-weight: 600;
-//   background-color: yellow;
-// `;
-
-// const Button = styled.button`
-//   font-size: 1.4rem;
-//   padding: 1.2rem 1.6rem;
-//   font-weight: 500;
-//   border: none;
-//   border-radius: var(--border-radius-sm);
-//   background-color: var(--color-brand-600);
-//   color: var(--color-brand-50);
-//   cursor: pointer;
-//   box-shadow: var(--shadow-sm);
-// `;
-
-// const Input = styled.input`
-//   border: 1px solid var(--color-grey-300);
-//   background-color: var(--color-grey-0);
-//   border-radius: var(--border-radius-sm);
-//   padding: 0.8rem 1.2rem;
-//   box-shadow: var(--shadow-sm);
-// `;
-
-const StyledApp = styled.main`
-  padding: 20px;
-`;
-
-// So what about styling App Component ?
 function App() {
   return (
-    // <div>
     <>
-      {/* Adding as a sibling to accept all global CSS */}
       <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">The Wild Oasis</Heading>
-            <div>
-              <Heading as="h2">Check in and out</Heading>
-              <Button
-                variation="primary"
-                size="medium"
-                onClick={() => alert("Check In")}
-              >
-                Check In
-              </Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("Check Out")}
-              >
-                Check Out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of Guests" />
-              <Input type="number" placeholder="Number of Guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          {/* Creating parent element as always */}
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<Users />} />
+            <Route path="settings" element={<Settings />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+          {/* Login and pagenotfound is not childs */}
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
-    // </div>
   );
 }
 
