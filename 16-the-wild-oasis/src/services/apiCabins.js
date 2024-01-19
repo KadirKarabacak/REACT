4b660b6cd3a5e721b182c3367152517e108d7293
Kadir <kadirht@hotmail.com>
2024-01-19 16:43:11 +0300

    createCabin function

--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -1,24 +1,38 @@
 import supabase from "./supabase";
 
 export async function getCabins() {
-  const { data, error } = await supabase.from("cabins").select("*");
+    const { data, error } = await supabase.from("cabins").select("*");
 
-  if (error) {
-    console.log(error.message);
-    throw new Error("Cabins can not loaded");
-  }
+    if (error) {
+        console.log(error.message);
+        throw new Error("Cabins can not loaded");
+    }
 
-  return data;
+    return data;
+}
+
+export async function createCabin(newCabin) {
+    const { data, error } = await supabase
+        .from("cabins")
+        .insert([newCabin])
+        .select();
+
+    if (error) {
+        console.log(error.message);
+        throw new Error("Cabin could not be deleted");
+    }
+
+    return data;
 }
 
 // Delete function which match the ID column with our id
 export async function deleteCabin(id) {
-  const { data, error } = await supabase.from("cabins").delete().eq("id", id);
+    const { data, error } = await supabase.from("cabins").delete().eq("id", id);
 
-  if (error) {
-    console.log(error.message);
-    throw new Error("Cabin could not be deleted");
-  }
+    if (error) {
+        console.log(error.message);
+        throw new Error("Cabin could not be deleted");
+    }
 
-  return data;
+    return data;
 }
