b94e7cec5db6c5e5d4ffcfbf9d1a98f62d0222d4
Kadir <kadirht@hotmail.com>
2024-01-19 16:42:48 +0300

    create cabin feature

--- 16-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 16-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -1,89 +1,145 @@
-import styled from "styled-components";
-
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
+import { createCabin } from "../../services/apiCabins";
+import FormRow from "../../ui/FormRow";
 
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import { useForm } from "react-hook-form";
+import { toast } from "react-hot-toast";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 function CreateCabinForm() {
-  return (
-    <Form>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
-        <Input type="text" id="name" />
-      </FormRow>
+    // React-hook-form handling controlled inputs, submit, reset, access of input values, formState object for errors
+    const { register, handleSubmit, reset, getValues, formState } = useForm();
+    const { errors } = formState;
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
-        <Input type="number" id="maxCapacity" />
-      </FormRow>
+    // Taking queryClient for invalidate cabins after add
+    const queryClient = useQueryClient();
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
-        <Input type="number" id="regularPrice" />
-      </FormRow>
+    // Taking mutation to mutate cabins
+    const { mutate, isLoading: isCreating } = useMutation({
+        mutationFn: createCabin,
+        onSuccess: () => {
+            toast.success("New cabin successfully created");
+            queryClient.invalidateQueries("cabins");
+            reset();
+        },
+        onError: err => {
+            toast.error(err.message);
+        },
+    });
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
-        <Input type="number" id="discount" defaultValue={0} />
-      </FormRow>
+    // Called on handleSubmit
+    function onSubmit(data) {
+        mutate(data);
+    }
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
-        <Textarea type="number" id="description" defaultValue="" />
-      </FormRow>
+    // If there is one matched validation it runs onError
+    function onError(errors) {
+        console.log(errors);
+    }
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
-      </FormRow>
+    return (
+        // We must call handleSubmit directly
+        <Form onSubmit={handleSubmit(onSubmit, onError)}>
+            <FormRow label="Cabin name" error={errors?.name?.message}>
+                <Input
+                    type="text"
+                    id="name"
+                    disabled={isCreating}
+                    {...register("name", {
+                        required: "Cabin name is required",
+                    })}
+                />
+            </FormRow>
 
-      <FormRow>
-        {/* type is an HTML attribute! */}
-        <Button variation="secondary" type="reset">
-          Cancel
-        </Button>
-        <Button>Edit cabin</Button>
-      </FormRow>
-    </Form>
-  );
+            <FormRow
+                label="Maximum capacity"
+                error={errors?.maxCapacity?.message}
+            >
+                <Input
+                    type="number"
+                    id="maxCapacity"
+                    disabled={isCreating}
+                    // doin register gives us some functions like onBlur, onChange
+                    {...register("maxCapacity", {
+                        required: "MaxCapacity is required",
+                        min: {
+                            value: 1,
+                            message: "Capacity should be at least 1",
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow
+                label="Regular price"
+                error={errors?.regularPrice?.message}
+            >
+                <Input
+                    type="number"
+                    id="regularPrice"
+                    disabled={isCreating}
+                    {...register("regularPrice", {
+                        required: "Regular Price is required",
+                        min: {
+                            value: 1,
+                            message: "Regular price should be at least 1",
+                        },
+                    })}
+                />
+            </FormRow>
+
+            <FormRow label="Discount" error={errors?.discount?.message}>
+                <Input
+                    type="number"
+                    id="discount"
+                    disabled={isCreating}
+                    defaultValue={0}
+                    {...register("discount", {
+                        required: "Discount is required",
+                        // Own validation function
+                        validate: value =>
+                            value <= getValues().regularPrice ||
+                            "Discount should be less than regular price",
+                    })}
+                />
+            </FormRow>
+
+            <FormRow
+                label="Description for website"
+                error={errors?.description?.message}
+            >
+                <Textarea
+                    type="number"
+                    id="description"
+                    disabled={isCreating}
+                    defaultValue=""
+                    {...register("description", {
+                        required: "Description is required",
+                    })}
+                />
+            </FormRow>
+
+            <FormRow label="Cabin photo">
+                <FileInput id="image" accept="image/*" {...register("image")} />
+            </FormRow>
+
+            <FormRow>
+                {/* type is an HTML attribute! */}
+                <Button
+                    disabled={isCreating}
+                    variation="secondary"
+                    type="reset"
+                >
+                    Cancel
+                </Button>
+                <Button disabled={isCreating}>Add Cabin</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default CreateCabinForm;
