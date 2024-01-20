9278949128d0cd1339e8bf7716c9be64a27c60d6
Kadir <kadirht@hotmail.com>
2024-01-20 20:24:40 +0300

    Created imagename and imagepath to upload cabin images

--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from "./supabase";
+import supabase, { supabaseUrl } from "./supabase";
 
 export async function getCabins() {
     const { data, error } = await supabase.from("cabins").select("*");
@@ -12,14 +12,37 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+    // We must replace all "/" with nothing to don't create any new folder in supabase
+    const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+        "/",
+        ""
+    );
+
+    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+    //  1. Create cabin
     const { data, error } = await supabase
         .from("cabins")
-        .insert([newCabin])
+        .insert([{ ...newCabin, image: imagePath }])
         .select();
 
     if (error) {
         console.log(error.message);
         throw new Error("Cabin could not be deleted");
+    }
+
+    // 2. If created cabin successfully, then upload image
+    const { error: storageError } = await supabase.storage
+        .from("cabin-images")
+        .upload(imageName, newCabin.image);
+
+    // 3. Delete cabin if there was an error.
+    if (storageError) {
+        await supabase.from("cabins").delete().eq("id", data.id);
+        console.log(storageError);
+        throw new Error(
+            "Cabin image could not be uploaded, and the cabin was not created"
+        );
     }
 
     return data;
