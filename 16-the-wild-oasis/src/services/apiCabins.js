38226bfb21a844b988bd8f7bf29d37cc98d4b1f4
Kadir <kadirht@hotmail.com>
2024-01-20 21:36:59 +0300

    feat: add edit cabin

--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -11,20 +11,39 @@ export async function getCabins() {
     return data;
 }
 
-export async function createCabin(newCabin) {
+// To reuse this func on edit and create
+export async function createEditCabin(newCabin, id) {
+    // On edit, we must check the image starts with the same url we created earlier
+    const hasImagePath = newCabin.image?.startsWith?.(supabaseUrl);
+
     // We must replace all "/" with nothing to don't create any new folder in supabase
     const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
         "/",
         ""
     );
 
-    const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+    // If has an imagepath on edit use it, otherwise create a new one
+    const imagePath = hasImagePath
+        ? newCabin.image
+        : `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
 
-    //  1. Create cabin
-    const { data, error } = await supabase
-        .from("cabins")
-        .insert([{ ...newCabin, image: imagePath }])
-        .select();
+    //  1. Create / Edit cabin
+    let query = supabase.from("cabins");
+
+    // A) CREATE
+    if (!id) query = query.insert([{ ...newCabin, image: imagePath }]);
+
+    // B) EDIT
+    if (id)
+        query = query
+            .update({ ...newCabin, image: imagePath })
+            .eq("id", id)
+            .select();
+
+    const { data, error } = await query
+        // To return newly created object, need to .select and .single
+        .select()
+        .single();
 
     if (error) {
         console.log(error.message);
