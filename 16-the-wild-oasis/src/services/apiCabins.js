2eb1de270e3cf498c97b8017e21b44628d754db6
Kadir <kadirht@hotmail.com>
2024-01-22 21:55:18 +0300

    fix: return early data

--- 16-the-wild-oasis/src/services/apiCabins.js
+++ 16-the-wild-oasis/src/services/apiCabins.js
@@ -51,6 +51,8 @@ export async function createEditCabin(newCabin, id) {
     }
 
     // 2. If created cabin successfully, then upload image
+    if (hasImagePath) return data;
+
     const { error: storageError } = await supabase.storage
         .from("cabin-images")
         .upload(imageName, newCabin.image);
