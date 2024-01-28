3eb64db6f272f6b89ea34f8ece496f2847372066
Kadir <kadirht@hotmail.com>
2024-01-29 02:22:27 +0300

    feat: tablerow feat

--- 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
+++ 16-the-wild-oasis/src/features/cabins/CabinRow.jsx
@@ -7,19 +7,7 @@ import { useDeleteCabin } from "./useDeleteCabin";
 import { useCreateCabin } from "./useCreateCabin.js";
 import Modal from "../../ui/Modal.jsx";
 import ConfirmDelete from "../../ui/ConfirmDelete.jsx";
-import { useContext } from "react";
-
-const TableRow = styled.div`
-    display: grid;
-    grid-template-columns: 0.6fr 1.8fr 2.2fr 1fr 1fr 1fr;
-    column-gap: 2.4rem;
-    align-items: center;
-    padding: 1.4rem 2.4rem;
-
-    &:not(:last-child) {
-        border-bottom: 1px solid var(--color-grey-100);
-    }
-`;
+import Table from "../../ui/Table.jsx";
 
 const Img = styled.img`
     display: block;
@@ -90,7 +78,7 @@ function CabinRow({ cabin }) {
     }
 
     return (
-        <TableRow role="row">
+        <Table.Row role="row">
             <Img src={image} alt="Cabin Image" />
             <Cabin>{name}</Cabin>
             <div>Fits up to {maxCapacity} guests</div>
@@ -128,7 +116,7 @@ function CabinRow({ cabin }) {
                     </Modal.Window>
                 </Modal>
             </div>
-        </TableRow>
+        </Table.Row>
     );
 }
 
