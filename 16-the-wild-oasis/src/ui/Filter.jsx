8f1d5009e5603b126422dfaac9d1b70f55ff6d64
Kadir <kadirht@hotmail.com>
2024-01-29 21:55:52 +0300

    fix: error fix about active prop

--- 16-the-wild-oasis/src/ui/Filter.jsx
+++ 16-the-wild-oasis/src/ui/Filter.jsx
@@ -16,8 +16,8 @@ const FilterButton = styled.button`
     background-color: var(--color-grey-0);
     border: none;
 
-    ${props =>
-        props.active &&
+    ${({ active }) =>
+        active &&
         css`
             background-color: var(--color-brand-600);
             color: var(--color-brand-50);
@@ -55,7 +55,8 @@ function Filter({ filterField, options }) {
                 <FilterButton
                     onClick={() => handleClick(option.value)}
                     key={option.value}
-                    active={option.value === currentFilter}
+                    active={option.value === currentFilter ? "true" : undefined}
+                    disabled={option.value === currentFilter}
                 >
                     {option.label}
                 </FilterButton>
