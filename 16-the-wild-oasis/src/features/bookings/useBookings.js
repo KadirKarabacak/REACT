a81e39dca587a94f4372f4fa2a008585468bb813
Kadir <kadirht@hotmail.com>
2024-02-02 20:26:27 +0300

    feat: server side filtering on bookins

--- 16-the-wild-oasis/src/features/bookings/useBookings.js
+++ 16-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,15 +1,28 @@
 // React Query
 import { useQuery } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
+import { useSearchParams } from "react-router-dom";
 
 export function useBookings() {
+    // We can get params right here to filter data with passing to getBookings
+    const [searchParams] = useSearchParams();
+
+    // FILTER
+    const filterValue = searchParams.get("status");
+    // To pass to getBookings
+    const filter =
+        !filterValue || filterValue === "all"
+            ? null
+            : { field: "status", value: filterValue };
+
     const {
         isLoading,
         data: bookings,
         error,
     } = useQuery({
-        queryKey: ["bookings"],
-        queryFn: getBookings,
+        // To make refetch on filter, we add our object to queryKey like useEffect
+        queryKey: ["bookings", filter],
+        queryFn: () => getBookings({ filter }),
     });
 
     return {
