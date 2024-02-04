b1e740da7ec7b8be8ec8d5b35956641f3f72d305
Kadir <kadirht@hotmail.com>
2024-02-04 17:01:52 +0300

    feat: add pagination

--- 16-the-wild-oasis/src/services/apiBookings.js
+++ 16-the-wild-oasis/src/services/apiBookings.js
@@ -1,32 +1,43 @@
+import { FIRST_PAGE, PAGE_SIZE } from "../utils/constants";
 import { getToday } from "../utils/helpers";
 import supabase from "./supabase";
 
-export async function getBookings({ filter, sortBy }) {
+export async function getBookings({ filter, sortBy, page }) {
     let query = supabase.from("bookings").select(
         `id, created_at, startDate, endDate,
              numNights, numGuests, status, totalPrice,
-             cabins(name), guests(fullName, email)`
+             cabins(name), guests(fullName, email)`,
+        { count: "exact" }
     );
     // .eq("status", "unconfirmed") // Equal
     // .gte("totalPrice", 2000); // Greater than
 
-    // Instead of hardcoding, we get conditionally fetching data with params
+    //! Filter
     if (filter)
         query = query[filter.method || "eq"](filter.field, filter.value);
 
+    //! Sort
     if (sortBy)
         query = query.order(sortBy.field, {
             ascending: sortBy.direction === "asc",
         });
 
-    const { data, error } = await query;
+    //! Page
+    if (page) {
+        const from = (page - FIRST_PAGE) * PAGE_SIZE;
+        const to = from + PAGE_SIZE - 1;
+        query = query.range(from, to);
+    }
+
+    // Count comes from into getBookings query
+    const { data, error, count } = await query;
 
     if (error) {
         console.error(error);
         throw new Error("Bookings could not get loaded");
     }
 
-    return data;
+    return { data, count };
 }
 
 export async function getBooking(id) {
