c6080ece85a1bcc0a1b4556f3998237e049536eb
Kadir <kadirht@hotmail.com>
2024-02-10 23:31:04 +0300

    feat: update user data

--- 16-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
+++ 16-the-wild-oasis/src/features/authentication/UpdateUserDataForm.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import toast from "react-hot-toast";
 
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
@@ -7,51 +8,79 @@ import FormRow from "../../ui/FormRow";
 import Input from "../../ui/Input";
 
 import { useUser } from "./useUser";
+import { useUpdateUser } from "./useUpdateUser";
 
 function UpdateUserDataForm() {
-  // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
-  const {
-    user: {
-      email,
-      user_metadata: { fullName: currentFullName },
-    },
-  } = useUser();
+    // We don't need the loading state, and can immediately use the user data, because we know that it has already been loaded at this point
+    const {
+        user: {
+            email,
+            user_metadata: { fullName: currentFullName },
+        },
+    } = useUser();
 
-  const [fullName, setFullName] = useState(currentFullName);
-  const [avatar, setAvatar] = useState(null);
+    const { updateUser, isUpdating } = useUpdateUser();
 
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
+    const [fullName, setFullName] = useState(currentFullName);
+    const [avatar, setAvatar] = useState(null);
 
-  return (
-    <Form onSubmit={handleSubmit}>
-      <FormRow label="Email address">
-        <Input value={email} disabled />
-      </FormRow>
-      <FormRow label="Full name">
-        <Input
-          type="text"
-          value={fullName}
-          onChange={(e) => setFullName(e.target.value)}
-          id="fullName"
-        />
-      </FormRow>
-      <FormRow label="Avatar image">
-        <FileInput
-          id="avatar"
-          accept="image/*"
-          onChange={(e) => setAvatar(e.target.files[0])}
-        />
-      </FormRow>
-      <FormRow>
-        <Button type="reset" variation="secondary">
-          Cancel
-        </Button>
-        <Button>Update account</Button>
-      </FormRow>
-    </Form>
-  );
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!fullName) return toast.error("Full name is required");
+        if (fullName && fullName === currentFullName)
+            return toast.error("Full name should be diffrent");
+        updateUser(
+            { fullName, avatar },
+            {
+                onSuccess: () => {
+                    // Resetting form after update
+                    setAvatar(null);
+                    e.target.reset();
+                },
+            }
+        );
+    }
+
+    function handleCancel() {
+        setFullName(currentFullName);
+        setAvatar(null);
+    }
+
+    return (
+        <Form onSubmit={handleSubmit}>
+            <FormRow label="Email address">
+                <Input value={email} disabled />
+            </FormRow>
+            <FormRow label="Full name">
+                <Input
+                    disabled={isUpdating}
+                    type="text"
+                    value={fullName}
+                    onChange={e => setFullName(e.target.value)}
+                    id="fullName"
+                />
+            </FormRow>
+            <FormRow label="Avatar image">
+                <FileInput
+                    disabled={isUpdating}
+                    id="avatar"
+                    accept="image/*"
+                    onChange={e => setAvatar(e.target.files[0])}
+                />
+            </FormRow>
+            <FormRow>
+                <Button
+                    disabled={isUpdating}
+                    type="reset"
+                    variation="secondary"
+                    onClick={handleCancel}
+                >
+                    Cancel
+                </Button>
+                <Button disabled={isUpdating}>Update account</Button>
+            </FormRow>
+        </Form>
+    );
 }
 
 export default UpdateUserDataForm;
