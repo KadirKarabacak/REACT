61c8be308fd07aa5ffa5789280f1a918439503c0
Kadir <kadirht@hotmail.com>
2024-02-11 20:39:24 +0300

    feat: dashboard layout dummy layout and custom hooks

--- 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -1,8 +1,35 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-template-rows: auto 34rem auto;
-  gap: 2.4rem;
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr 1fr;
+    grid-template-rows: auto 34rem auto;
+    gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+    const { bookings, isLoading: isLoadingBookings } = useRecentBookings();
+    console.log(bookings);
+
+    const {
+        stays,
+        confirmedStays,
+        isLoading: isLoadingStays,
+    } = useRecentStays();
+
+    if (isLoadingBookings || isLoadingStays) return <Spinner />;
+
+    return (
+        <StyledDashboardLayout>
+            <div>Statistics</div>
+            <div>Today&lsquo;s activity</div>
+            <div>Chart stay duration</div>
+            <div>Chart sales</div>
+        </StyledDashboardLayout>
+    );
+}
+
+export default DashboardLayout;
