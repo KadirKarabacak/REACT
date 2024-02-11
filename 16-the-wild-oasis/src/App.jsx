b69a1a7327c908b46c520bdc8369cafb1eee5174
Kadir <kadirht@hotmail.com>
2024-02-11 16:04:45 +0300

    feat: darkmode provider

--- 16-the-wild-oasis/src/App.jsx
+++ 16-the-wild-oasis/src/App.jsx
@@ -21,6 +21,7 @@ import ProtectedRoute from "./ui/ProtectedRoute";
 import GlobalStyles from "./styles/GlobalStyles";
 import Booking from "./pages/Booking";
 import Checkin from "./pages/Checkin";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 // Creating new QueryClient for React-Query
 const queryClient = new QueryClient({
@@ -34,47 +35,49 @@ const queryClient = new QueryClient({
 
 function App() {
     return (
-        // Providing our queryClient
-        <QueryClientProvider client={queryClient}>
-            {/* React-Query-Devtools*/}
-            <ReactQueryDevtools initialIsOpen={false} />
-            {/* Global Styles */}
-            <GlobalStyles />
-            <BrowserRouter>
-                <Routes>
-                    {/* Creating parent element as always */}
-                    <Route
-                        element={
-                            <ProtectedRoute>
-                                <AppLayout />
-                            </ProtectedRoute>
-                        }
-                    >
+        <DarkModeProvider>
+            {/* Providing our queryClient */}
+            <QueryClientProvider client={queryClient}>
+                {/* React-Query-Devtools*/}
+                <ReactQueryDevtools initialIsOpen={false} />
+                {/* Global Styles */}
+                <GlobalStyles />
+                <BrowserRouter>
+                    <Routes>
+                        {/* Creating parent element as always */}
                         <Route
-                            index
-                            element={<Navigate replace to="dashboard" />}
-                        />
-                        <Route path="dashboard" element={<Dashboard />} />
-                        <Route path="bookings" element={<Bookings />} />
-                        <Route
-                            path="bookings/:bookingId"
-                            element={<Booking />}
-                        />
-                        <Route
-                            path="checkin/:bookingId"
-                            element={<Checkin />}
-                        />
-                        <Route path="cabins" element={<Cabins />} />
-                        <Route path="users" element={<Users />} />
-                        <Route path="settings" element={<Settings />} />
-                        <Route path="account" element={<Account />} />
-                    </Route>
-                    {/* Login and pagenotfound is not childs */}
-                    <Route path="login" element={<Login />} />
-                    <Route path="*" element={<PageNotFound />} />
-                </Routes>
-            </BrowserRouter>
-        </QueryClientProvider>
+                            element={
+                                <ProtectedRoute>
+                                    <AppLayout />
+                                </ProtectedRoute>
+                            }
+                        >
+                            <Route
+                                index
+                                element={<Navigate replace to="dashboard" />}
+                            />
+                            <Route path="dashboard" element={<Dashboard />} />
+                            <Route path="bookings" element={<Bookings />} />
+                            <Route
+                                path="bookings/:bookingId"
+                                element={<Booking />}
+                            />
+                            <Route
+                                path="checkin/:bookingId"
+                                element={<Checkin />}
+                            />
+                            <Route path="cabins" element={<Cabins />} />
+                            <Route path="users" element={<Users />} />
+                            <Route path="settings" element={<Settings />} />
+                            <Route path="account" element={<Account />} />
+                        </Route>
+                        {/* Login and pagenotfound is not childs */}
+                        <Route path="login" element={<Login />} />
+                        <Route path="*" element={<PageNotFound />} />
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
+        </DarkModeProvider>
     );
 }
 
