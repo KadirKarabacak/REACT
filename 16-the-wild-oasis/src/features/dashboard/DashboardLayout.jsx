c53b577627edf065278e47588a1fdd2a3c905a1e
Kadir <kadirht@hotmail.com>
2024-02-12 00:21:45 +0300

    feat: add duration and sales chart

--- 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
+++ 16-the-wild-oasis/src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,8 @@ import Spinner from "../../ui/Spinner";
 import { useRecentStays } from "./useRecentStays";
 import Stats from "./Stats";
 import { useCabins } from "../cabins/useCabins";
+import SalesChart from "./SalesChart";
+import DurationChart from "./DurationChart";
 
 const StyledDashboardLayout = styled.div`
     display: grid;
@@ -36,6 +38,9 @@ function DashboardLayout() {
                 numDays={numDays}
                 cabinCount={cabins.length}
             />
+            <div>Todays activity</div>
+            <DurationChart confirmedStays={confirmedStays} />
+            <SalesChart bookings={bookings} numDays={numDays} />
         </StyledDashboardLayout>
     );
 }
