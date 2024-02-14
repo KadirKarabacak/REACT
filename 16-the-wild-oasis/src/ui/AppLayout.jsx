5d10540375d8ee34bf296075b9df768e12d7f393
Kadir <kadirht@hotmail.com>
2024-02-15 00:06:41 +0300

    fix: styling fix

--- 16-the-wild-oasis/src/ui/AppLayout.jsx
+++ 16-the-wild-oasis/src/ui/AppLayout.jsx
@@ -14,6 +14,7 @@ const StyledAppLayout = styled.div`
 const Main = styled.main`
     background-color: var(--color-grey-50);
     padding: 4rem 4.8rem 6.4rem;
+    overflow: scroll;
 `;
 
 const Container = styled.div`
@@ -36,7 +37,7 @@ function AppLayout() {
                 </Container>
             </Main>
             <Toaster
-                position="top-right"
+                position="top-center"
                 gutter={12}
                 containerStyle={{ margin: "8px" }}
                 toastOptions={{
