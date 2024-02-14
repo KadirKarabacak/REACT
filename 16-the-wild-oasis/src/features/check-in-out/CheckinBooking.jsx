38b080ee55f581963b5724955cb87d8073ee3906
Kadir <kadirht@hotmail.com>
2024-02-15 00:05:37 +0300

    fix error fix

--- 16-the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
+++ 16-the-wild-oasis/src/features/check-in-out/CheckinBooking.jsx
@@ -49,7 +49,7 @@ function CheckinBooking() {
     } = booking;
 
     const optionalBreakfastPrice =
-        settings.breakfastPrice * numNights * numGuests;
+        settings?.breakfastPrice * numNights * numGuests;
 
     function handleCheckin() {
         if (!confirmPaid) return;
