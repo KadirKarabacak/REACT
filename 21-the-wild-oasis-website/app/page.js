e14fc03499f641ec232fe207e8fd9843bba13a5a
Kadir <kadirht@hotmail.com>
2024-06-05 17:18:06 +0300

    chore: remove navigation to layout

--- 21-the-wild-oasis-website/app/page.js
+++ 21-the-wild-oasis-website/app/page.js
@@ -4,7 +4,6 @@ import Navigation from "./components/Navigation";
 export default function Page() {
     return (
         <div>
-            <Navigation />
             <h1>Hello Next!</h1>
         </div>
     );
