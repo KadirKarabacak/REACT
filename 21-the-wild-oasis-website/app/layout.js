9b055c756c13b13ff9305e33a0bf70d7e2d98abc
Kadir <kadirht@hotmail.com>
2024-06-22 18:21:34 +0300

    feat: include header component

--- 21-the-wild-oasis-website/app/layout.js
+++ 21-the-wild-oasis-website/app/layout.js
@@ -9,6 +9,7 @@ const josefin = Josefin_Sans({
 });
 
 import "@/app/_styles/globals.css";
+import Header from "./_components/Header";
 
 // Page title witch shown on new browser tab
 export const metadata = {
@@ -25,14 +26,12 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body
-                className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}
+                className={`${josefin.className} bg-primary-950 antialiased text-primary-100 min-h-screen flex flex-col`}
             >
-                <header>
-                    <Logo />
-                </header>
-                <Navigation />
-                <main>{children}</main>
-                <footer>Copyright by The Wild Oasis</footer>
+                <Header />
+                <div className="flex-1 px-8 py-12">
+                    <main className="max-w-7xl mx-auto">{children}</main>
+                </div>
             </body>
         </html>
     );
