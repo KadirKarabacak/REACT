321ddd4471f0a32c0749526d8a2d994a75bd3ce7
Kadir <kadirht@hotmail.com>
2024-06-26 23:42:44 +0300

    feat: update next/config to fix database image

--- 21-the-wild-oasis-website/next.config.mjs
+++ 21-the-wild-oasis-website/next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+    // When we get images from a database, nextJs warn us to spesify these things.
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "mcgxonulxclcyrtynujv.supabase.co",
+                port: "",
+                pathname: "/storage/v1/object/public/cabin-images/**",
+            },
+        ],
+    },
+};
 
 export default nextConfig;
