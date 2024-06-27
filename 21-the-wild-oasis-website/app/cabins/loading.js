859905fc3e92aef6d9ed3ee3d643cfc32ba00494
Kadir <kadirht@hotmail.com>
2024-06-27 22:55:25 +0300

    chore: add comments

--- 21-the-wild-oasis-website/app/cabins/loading.js
+++ 21-the-wild-oasis-website/app/cabins/loading.js
@@ -1,5 +1,6 @@
 import Spinner from "../_components/Spinner";
 
+// This loading file blocks whole cabins route while fetching data. For more spesific loader use Suspense
 export default function Loading() {
     return (
         <div className="grid items-center justify-center">
