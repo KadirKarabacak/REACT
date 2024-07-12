03a6a1642f85cc76d1ea86ff6c5873d5e1ff001a
KadirKarabacak <kadirht@hotmail.com>
2024-07-13 00:51:45 +0300

    feat: import and render client component in to server component

--- 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
+++ 21-the-wild-oasis-website/app/cabins/[cabinId]/page.js
@@ -1,3 +1,4 @@
+import TextExpander from "@/app/_components/TextExpander";
 import { getCabin, getCabins } from "@/app/_lib/data-service";
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from "@heroicons/react/24/solid";
 import Image from "next/image";
@@ -18,7 +19,7 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }) {
-    const cabin = await getCabin(params.cabinId);
+    const cabin = await getCabin(params?.cabinId);
     const {
         id,
         name,
@@ -49,7 +50,7 @@ export default async function Page({ params }) {
                     </h3>
 
                     <p className="text-lg text-primary-300 mb-10">
-                        {description}
+                        <TextExpander>{description}</TextExpander>
                     </p>
 
                     <ul className="flex flex-col gap-4 mb-7">
