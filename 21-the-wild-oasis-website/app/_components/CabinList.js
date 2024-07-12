7042cd135dd12106c40b9319ed6803757e237f7c
KadirKarabacak <kadirht@hotmail.com>
2024-07-13 00:50:32 +0300

    chore: add guard closures

--- 21-the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -1,17 +1,16 @@
 import CabinCard from "@/app/_components/CabinCard";
 import { getCabins } from "@/app/_lib/data-service";
-// import { unstable_noStore as noStore } from "next/cache";
+import { unstable_noStore as noStore } from "next/cache";
 
 export default async function CabinList() {
     // With this, Cabins page become dynamic again, cuz we are not caching component
-    // noStore();
+    noStore();
     const cabins = await getCabins();
 
-    if (!cabins.length) return null;
-
+    if (!cabins?.length) return null;
     return (
         <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-            {cabins.map(cabin => (
+            {cabins?.map(cabin => (
                 <CabinCard cabin={cabin} key={cabin.id} />
             ))}
         </div>
