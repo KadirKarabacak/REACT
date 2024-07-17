f4cc0ec689b4750ac9c36faccededd7627864e58
KadirKarabacak <kadirht@hotmail.com>
2024-07-18 00:59:42 +0300

    feat: create reservation comp to fetch only necessary data

---
+++ 21-the-wild-oasis-website/app/_components/Reservation.js
@@ -0,0 +1,24 @@
+import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
+import DateSelector from "./DateSelector";
+import ReservationForm from "./ReservationForm";
+
+async function Reservation({ cabin }) {
+    //! To solve this problem we can use Promise.All. But this method just fast as possible as slowest promise.
+    const [settings, bookedDates] = await Promise.all([
+        getSettings(),
+        getBookedDatesByCabinId(cabin.id),
+    ]);
+    return (
+        <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
+            {/* As a convention client components must be take less data, not like whole object. */}
+            <DateSelector
+                settings={settings}
+                bookedDates={bookedDates}
+                cabin={cabin}
+            />
+            <ReservationForm />
+        </div>
+    );
+}
+
+export default Reservation;
