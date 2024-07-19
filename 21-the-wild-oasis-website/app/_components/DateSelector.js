99efeaebe1acab0e2191ab3267e6bf9b3926cda4
KadirKarabacak <kadirht@hotmail.com>
2024-07-19 18:47:21 +0300

    feat: make dateselector controlled & get state from context

--- 21-the-wild-oasis-website/app/_components/DateSelector.js
+++ 21-the-wild-oasis-website/app/_components/DateSelector.js
@@ -2,8 +2,10 @@
 
 // React day picker requires use client
 import { isWithinInterval } from "date-fns";
+import { useState } from "react";
 import { DayPicker } from "react-day-picker";
 import "react-day-picker/dist/style.css";
+import { useReservation } from "./ReservationContext";
 
 function isAlreadyBooked(range, datesArr) {
     return (
@@ -16,12 +18,13 @@ function isAlreadyBooked(range, datesArr) {
 }
 
 function DateSelector({ settings, bookedDates, cabin }) {
+    const { range, setRange, resetRange } = useReservation();
     // CHANGE
     const regularPrice = 23;
     const discount = 23;
     const numNights = 23;
     const cabinPrice = 23;
-    const range = { from: null, to: null };
+    // const range = { from: null, to: null };
 
     // SETTINGS
     const { minBookingLength, maxBookingLength } = cabin;
@@ -33,6 +36,8 @@ function DateSelector({ settings, bookedDates, cabin }) {
                 mode="range"
                 min={minBookingLength + 1}
                 max={maxBookingLength}
+                onSelect={range => setRange(range)}
+                selected={range}
                 fromMonth={new Date()}
                 fromDate={new Date()}
                 toYear={new Date().getFullYear() + 5}
@@ -77,7 +82,7 @@ function DateSelector({ settings, bookedDates, cabin }) {
                 {range.from || range.to ? (
                     <button
                         className="border border-primary-800 py-2 px-4 text-sm font-semibold"
-                        onClick={() => resetRange()}
+                        onClick={resetRange}
                     >
                         Clear
                     </button>
