c22f388b59dfbfaaf87a6470fd21d4f5f43dd147
KadirKarabacak <kadirht@hotmail.com>
2024-07-25 21:06:00 +0300

    feat: use next auth to display conditional form

--- 21-the-wild-oasis-website/app/_components/Reservation.js
+++ 21-the-wild-oasis-website/app/_components/Reservation.js
@@ -1,5 +1,7 @@
+import { auth } from "../_lib/auth";
 import { getBookedDatesByCabinId, getSettings } from "../_lib/data-service";
 import DateSelector from "./DateSelector";
+import LoginMessage from "./LoginMessage";
 import ReservationForm from "./ReservationForm";
 
 async function Reservation({ cabin }) {
@@ -8,6 +10,8 @@ async function Reservation({ cabin }) {
         getSettings(),
         getBookedDatesByCabinId(cabin.id),
     ]);
+
+    const session = await auth();
     return (
         <div className="grid grid-cols-2 border border-primary-800 min-h-[400px]">
             {/* As a convention client components must be take less data, not like whole object. */}
@@ -16,7 +20,11 @@ async function Reservation({ cabin }) {
                 bookedDates={bookedDates}
                 cabin={cabin}
             />
-            <ReservationForm cabin={cabin} />
+            {session?.user ? (
+                <ReservationForm user={session.user} cabin={cabin} />
+            ) : (
+                <LoginMessage />
+            )}
         </div>
     );
 }
