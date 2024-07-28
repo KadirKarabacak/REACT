0235e8d341907677677450f8b4d368c1af9a7271
KadirKarabacak <kadirht@hotmail.com>
2024-07-28 23:41:58 +0300

    feat: update middleware with auth.

--- 21-the-wild-oasis-website/middleware.js
+++ 21-the-wild-oasis-website/middleware.js
@@ -1,13 +1,19 @@
-import { NextResponse } from "next/server";
+//! Just for referance, instead we use Auth.js
+// import { NextResponse } from "next/server";
 
-export function middleware(request) {
-    console.log(request);
+import { auth } from "./app/_lib/auth";
 
-    // This redirect makes a loop of requests
-    return NextResponse.redirect(new URL("/about", request.url));
-}
+// export function middleware(request) {
+//     console.log(request);
 
-// To fix loop of redirects
+//     // This redirect makes a loop of requests
+//     return NextResponse.redirect(new URL("/about", request.url));
+// }
+
+//! We exporting middleware as auth which comes from NextAuth
+export const middleware = auth;
+
+//! To spesify which routes must be protected
 export const config = {
-    matcher: ["/account", "/cabins"],
+    matcher: ["/account"],
 };
