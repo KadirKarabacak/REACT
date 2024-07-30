1a31f350b6341ab55667901c7de22dbde365b4e9
KadirKarabacak <kadirht@hotmail.com>
2024-07-31 01:09:01 +0300

    fix: fix styling

--- 21-the-wild-oasis-website/app/_components/CabinList.js
+++ 21-the-wild-oasis-website/app/_components/CabinList.js
@@ -20,7 +20,7 @@ export default async function CabinList({ filter }) {
 
     if (!cabins?.length) return null;
     return (
-        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8">
             {displayedCabins?.map(cabin => (
                 <CabinCard cabin={cabin} key={cabin.id} />
             ))}
