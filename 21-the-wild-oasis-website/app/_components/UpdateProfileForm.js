36fcc01ee9ab2a0c0a2b193a99670ac49c1e88ac
KadirKarabacak <kadirht@hotmail.com>
2024-08-05 19:00:18 +0300

    feat: add server actions to client comp & name fields to update data

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -1,19 +1,26 @@
 "use client";
 // This one is a client component trying to import a server component [ SelectCountry ]. Trying this makes us problems
 import React, { useState } from "react";
+import { updateGuest } from "../_lib/actions";
 
 //! Importing a server component inside client component causing the problem
 // import SelectCountry from "./SelectCountry";
 
-function UpdateProfileForm({ children }) {
+function UpdateProfileForm({ children, guest }) {
     const [count, setCount] = useState();
-    const countryFlag = "pt.jpg";
+
+    const { fullName, email, nationality, nationalID, countryFlag } = guest;
 
     return (
-        <form className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col">
+        <form
+            action={updateGuest}
+            className="bg-primary-900 py-8 px-12 text-lg flex gap-6 flex-col"
+        >
             <div className="space-y-2">
                 <label>Full name</label>
                 <input
+                    name="fullName"
+                    defaultValue={fullName}
                     disabled
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
                 />
@@ -22,6 +29,8 @@ function UpdateProfileForm({ children }) {
             <div className="space-y-2">
                 <label>Email address</label>
                 <input
+                    name="email"
+                    defaultValue={email}
                     disabled
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm disabled:cursor-not-allowed disabled:bg-gray-600 disabled:text-gray-400"
                 />
@@ -41,6 +50,7 @@ function UpdateProfileForm({ children }) {
             <div className="space-y-2">
                 <label htmlFor="nationalID">National ID number</label>
                 <input
+                    defaultValue={nationalID}
                     name="nationalID"
                     className="px-5 py-3 bg-primary-200 text-primary-800 w-full shadow-sm rounded-sm"
                 />
