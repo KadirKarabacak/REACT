40bc0f4dd9acb8bdc1d8bd991aab074467f4967a
KadirKarabacak <kadirht@hotmail.com>
2024-08-06 22:41:11 +0300

    feat: implement use form status to handle form's loading situation

--- 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
+++ 21-the-wild-oasis-website/app/_components/UpdateProfileForm.js
@@ -2,6 +2,7 @@
 // This one is a client component trying to import a server component [ SelectCountry ]. Trying this makes us problems
 import React, { useState } from "react";
 import { updateGuest } from "../_lib/actions";
+import { useFormStatus } from "react-dom";
 
 //! Importing a server component inside client component causing the problem
 // import SelectCountry from "./SelectCountry";
@@ -57,11 +58,23 @@ function UpdateProfileForm({ children, guest }) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+                <Button />
             </div>
         </form>
+    );
+}
+
+function Button() {
+    // Pending status holds form's current situation
+    const { pending, formData, action } = useFormStatus();
+
+    return (
+        <button
+            disabled={pending}
+            className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
+        >
+            {pending ? "Updating..." : "Update profile"}
+        </button>
     );
 }
 
