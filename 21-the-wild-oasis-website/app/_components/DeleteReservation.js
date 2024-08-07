b7b68a7faadf78ad40244548e73f9ef6c4bc9d27
KadirKarabacak <kadirht@hotmail.com>
2024-08-08 00:59:02 +0300

    feat: add delete reservation feature by server action and use usetransition hook to handle loading indicators

--- 21-the-wild-oasis-website/app/_components/DeleteReservation.js
+++ 21-the-wild-oasis-website/app/_components/DeleteReservation.js
@@ -1,12 +1,43 @@
-import { TrashIcon } from '@heroicons/react/24/solid';
+// This comp must be client cuz of we invoke server action directly in button click instead of using a form
+"use client";
+import { TrashIcon } from "@heroicons/react/24/solid";
+import { deleteReservation } from "../_lib/actions";
+import { useTransition } from "react";
+import SpinnerMini from "./SpinnerMini";
 
 function DeleteReservation({ bookingId }) {
-  return (
-    <button className='group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900'>
-      <TrashIcon className='h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors' />
-      <span className='mt-1'>Delete</span>
-    </button>
-  );
+    // For delete reservation we can create server action right here.
+    // function deleteReservation() {
+    // To create a server action right here, we must use "use server" directive top of that function to be sure always this func stays on server
+    //     "use server";
+    // }
+
+    const [isPending, startTransition] = useTransition();
+
+    function handleDelete() {
+        if (confirm("Are you sure you want to delete this reservation?"))
+            // Wrapping our action with startTransition to mark it as transition
+            startTransition(() => deleteReservation(bookingId));
+    }
+
+    return (
+        <button
+            disabled={isPending}
+            onClick={handleDelete}
+            className="group flex items-center gap-2 uppercase text-xs font-bold text-primary-300 flex-grow px-3 hover:bg-accent-600 transition-colors hover:text-primary-900"
+        >
+            {!isPending ? (
+                <>
+                    <span className="mt-1">Delete</span>
+                    <TrashIcon className="h-5 w-5 text-primary-600 group-hover:text-primary-800 transition-colors" />
+                </>
+            ) : (
+                <span className="mx-auto">
+                    <SpinnerMini />
+                </span>
+            )}
+        </button>
+    );
 }
 
 export default DeleteReservation;
