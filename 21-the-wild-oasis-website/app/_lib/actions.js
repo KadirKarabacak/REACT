b30c3d7f88ff438f3cc77e2f11d3ae0b6e477d5c
KadirKarabacak <kadirht@hotmail.com>
2024-08-08 01:00:02 +0300

    feat: create delete reservation server action

--- 21-the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/app/_lib/actions.js
@@ -1,10 +1,11 @@
 //! Server actions must be created in "use server" directive. It's like a bridge from going to the client back to the server
+//! We must use our own signIn function which we take and export from NextAuth
 "use server";
 
 import { revalidatePath } from "next/cache";
-//! We must use our own signIn function which we take and export from NextAuth
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 const nationalIDRegex = /^[a-zA-Z0-9]{6,12}$/;
 
@@ -30,6 +31,29 @@ export async function updateGuest(formData) {
 
     // After the mutation done, revalidatePath to re-render route
     revalidatePath("/account/profile");
+}
+
+export async function deleteReservation(bookingId) {
+    const session = await auth();
+    if (!session) throw new Error("You must be logged in");
+
+    // Any user can delete other bookings using any of our requests in network tab with copying request as cURL
+    // https://www.udemy.com/course/the-ultimate-react-course/learn/lecture/43783896#questions   14:00
+    //! To fix that problem up there ->>
+    // const guestBookings = await getBookings(bookingId);
+    // const guestBookingIds = guestBookings.map(booking => booking.id);
+    // if (!guestBookingIds.includes(bookingId))
+    //     throw new Error("You are not allowed to delete this booking.");
+
+    const { error } = await supabase
+        .from("bookings")
+        .delete()
+        .eq("id", bookingId);
+
+    if (error) throw new Error("Booking could not be deleted");
+
+    // After the mutation done, revalidatePath to re-render route
+    revalidatePath("/account/reservations");
 }
 
 export async function signInAction() {
