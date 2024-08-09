31abee5c0990517607c6465cc6c3431e5f527ed2
KadirKarabacak <kadirht@hotmail.com>
2024-08-09 10:44:24 +0300

    feat: add update reservation action & programmatic navigation

--- 21-the-wild-oasis-website/app/_lib/actions.js
+++ 21-the-wild-oasis-website/app/_lib/actions.js
@@ -1,11 +1,13 @@
 //! Server actions must be created in "use server" directive. It's like a bridge from going to the client back to the server
 //! We must use our own signIn function which we take and export from NextAuth
+//! No way to get params on URL in Server Actions
 "use server";
 
 import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
 import { getBookings } from "./data-service";
+import { redirect } from "next/navigation";
 
 const nationalIDRegex = /^[a-zA-Z0-9]{6,12}$/;
 
@@ -39,11 +41,11 @@ export async function deleteReservation(bookingId) {
 
     // Any user can delete other bookings using any of our requests in network tab with copying request as cURL
     // https://www.udemy.com/course/the-ultimate-react-course/learn/lecture/43783896#questions   14:00
-    //! To fix that problem up there ->>
-    // const guestBookings = await getBookings(bookingId);
-    // const guestBookingIds = guestBookings.map(booking => booking.id);
-    // if (!guestBookingIds.includes(bookingId))
-    //     throw new Error("You are not allowed to delete this booking.");
+    //! To fix that problem
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings?.map(booking => booking.id);
+    if (!guestBookingIds.includes(bookingId))
+        throw new Error("You are not allowed to delete this booking.");
 
     const { error } = await supabase
         .from("bookings")
@@ -54,6 +56,41 @@ export async function deleteReservation(bookingId) {
 
     // After the mutation done, revalidatePath to re-render route
     revalidatePath("/account/reservations");
+}
+
+export async function updateReservation(formData) {
+    // 1) Authentication
+    const session = await auth();
+    if (!session)
+        throw new Error("You must be logged in to update reservation");
+
+    // 3) Building update data
+    const bookingId = Number(formData.get("bookingId"));
+    const observations = formData.get("observations").slice(0, 1000);
+    const numGuests = Number(formData.get("numGuests"));
+    const updatedFields = { observations, numGuests };
+
+    // 2) Authorization
+    const guestBookings = await getBookings(session.user.guestId);
+    const guestBookingIds = guestBookings?.map(booking => booking.id);
+    if (!guestBookingIds.includes(bookingId))
+        throw new Error("You are not allowed to delete this booking.");
+
+    // 4) Mutation
+    const { error } = await supabase
+        .from("bookings")
+        .update(updatedFields)
+        .eq("id", bookingId);
+
+    // 5) Error handling
+    if (error) throw new Error("Booking could not be updated");
+
+    // 6) Revalidating
+    revalidatePath(`/account/reservations/edit/${bookingId}`);
+    revalidatePath("/account/reservations");
+
+    // 7) Redirecting
+    redirect("/account/reservations");
 }
 
 export async function signInAction() {
